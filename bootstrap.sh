acf8baad7a6ed64d318175c9e0251f5bcc6fabaa
Tim Retz <human.shield.117@gmail.com>
2022-10-22 10:47:33 -0700

    Add my custom fonts

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,7 @@ ln -s ~/src/dotfiles/neovim_config-lua/ ~/.config/nvim
 
 # SymLink git master configurations
 ln -s ~/src/dotfiles/gitconfig/ ~/.config/git
+
+# SymLink my prefered fonts, and then update cache
+ln -s ~/src/dotfiles/fonts ~/.fonts
+fc-cache -f -v
