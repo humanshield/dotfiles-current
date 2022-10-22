f444bb69b22ae0939842ae3c6136f05d23b29173
Tim Retz <human.shield.117@gmail.com>
2022-10-22 10:28:52 -0700

    fixed bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,7 @@
 
 # SymLink neovim configuration
 #   No need for anything extra
-ln -s neovim_config-lua/ ~/.config/nvim/
+ln -s ~/src/dotfiles/neovim_config-lua/ ~/.config/nvim
+
+# SymLink git master configurations
+ln -s ~/src/dotfiles/gitconfig/ ~/.config/git
